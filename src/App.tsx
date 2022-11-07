52bd05cda7fc8294a56810bc64e6d4340b514214
cros6y6raga <crosbybraga@gmail.com>
2022-11-07 22:33:58 +0300

    Creating a second Tudulist

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
 import React from 'react';
 import './App.css';
+import TodoList from "./TodoList";
+
+export type TasksType = {
+    id: number
+    title: string
+    isDone: boolean
+}
 
 function App() {
+    const todoListTitle_1: string = 'What to learn'
+    const todoListTitle_2: string = 'What to buy'
+    const tasks_1: Array<TasksType> = [
+        {id: 1, title: 'HTML & CSS', isDone: true},
+        {id: 2, title: 'JS', isDone: true},
+        {id: 3, title: 'React', isDone: false},
+    ]
+    const tasks_2: Array<TasksType> = [
+        {id: 4, title: 'Beer', isDone: false},
+        {id: 5, title: 'Wine', isDone: true},
+        {id: 6, title: 'Skotch', isDone: true},
+    ]
     return (
         <div className="App">
-            <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
+            <TodoList title={todoListTitle_1} tasks={tasks_1}/>
+            <TodoList title={todoListTitle_2} tasks={tasks_2}/>
         </div>
     );
 }

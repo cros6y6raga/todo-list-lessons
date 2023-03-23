91e60b1d1e5a911a829f197eea63d57de4f8adf2
cros6y6raga <crosbybraga@gmail.com>
2023-03-23 13:00:00 +0300

    Drawing an AddItemForm in storybook

--- src/stories/AddItemForm.stories.tsx
+++ src/stories/AddItemForm.stories.tsx
@@ -1,41 +1,25 @@
 import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import {ComponentStory, ComponentMeta} from '@storybook/react';
 
-import { Button } from './Button';
+import {Button} from './Button';
+import {AddItemForm} from "../AddItemForm";
+import {action} from "@storybook/addon-actions";
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: 'Example/Button',
-  component: Button,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
-} as ComponentMeta<typeof Button>;
+    title: 'TODOLISTS/AddItemForm',
+    component: AddItemForm,
+    // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+    argTypes: {
+        addItem: {description: 'Button clicked inside form'},
+    },
+} as ComponentMeta<typeof AddItemForm>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+const Template: ComponentStory<typeof AddItemForm> = (args) => <AddItemForm {...args} />;
 
-export const Primary = Template.bind({});
+export const AddItemFormStory = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
-Primary.args = {
-  primary: true,
-  label: 'Button',
-};
-
-export const Secondary = Template.bind({});
-Secondary.args = {
-  label: 'Button',
-};
-
-export const Large = Template.bind({});
-Large.args = {
-  size: 'large',
-  label: 'Button',
-};
-
-export const Small = Template.bind({});
-Small.args = {
-  size: 'small',
-  label: 'Button',
+AddItemFormStory.args = {
+    addItem: action('Button clicked inside form')
 };

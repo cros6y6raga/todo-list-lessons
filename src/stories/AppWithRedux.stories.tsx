0c585408f019f43cae225e6e9f3198e40ed05032
cros6y6raga <crosbybraga@gmail.com>
2023-03-24 12:45:03 +0300

    Decorator

--- src/stories/AppWithRedux.stories.tsx
+++ src/stories/AppWithRedux.stories.tsx
@@ -12,19 +12,17 @@ import {TaskType} from "../Todolist";
 import AppWithRedux from "../AppWithRedux";
 import {Provider} from "react-redux";
 import {store} from "../state/store";
+import {ReduxStoreProviderDecorator} from "../state/ReduxStoreProviderDecorator";
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
     title: 'TODOLISTS/AppWithRedux',
     component: AppWithRedux,
     // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-} as ComponentMeta<typeof Task>;
+    decorators: [ReduxStoreProviderDecorator]
+} as ComponentMeta<typeof AppWithRedux>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof AppWithRedux> = (args) => <Provider store={store}><AppWithRedux/></Provider>;
-
-export const TaskIsDoneStory = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
-
+const Template: ComponentStory<typeof AppWithRedux> = (args) => <AppWithRedux/>
 
 export const AppWithReduxStory = Template.bind({});
